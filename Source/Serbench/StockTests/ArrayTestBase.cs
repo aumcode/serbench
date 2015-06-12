a9b570bd4472a2edc9b33bef867d38b97af1c89c
Dmitriy@ITAdapter <dmitriy@itadapter.com>
2015-06-12 16:16:08 -0400

    Added array serializer tests

---
+++ Source/Serbench/StockTests/ArrayTestBase.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+using NFX;
+using NFX.Environment;
+using NFX.Parsing;
+
+namespace Serbench.StockTests
+{
+
+  public abstract class ArrayTestBase : Test
+  {
+    public const string CONFIG_DIMENSIONS_ATTR = "dimensions";
+
+    protected ArrayTestBase(TestingSystem context, IConfigSectionNode conf) : base(context, conf) 
+    { 
+      var dims = conf.AttrByName(CONFIG_DIMENSIONS_ATTR).Value;
+      
+      if (dims.IsNullOrWhiteSpace())
+        throw new SerbenchException("Array dimensions attribute '{0}' is not specified".Args(CONFIG_DIMENSIONS_ATTR));
+
+      m_Dimensions = dims.Split(',',';').Where(s => s.IsNotNullOrWhiteSpace())
+                                        .Select(s => s.AsInt())
+                                        .ToArray();  
+      
+      if (m_Dimensions.Length<1 || m_Dimensions.Any(d => d<=0))
+       throw new SerbenchException("Invalid array dimensions attribute '{0}' = '{1}'".Args(CONFIG_DIMENSIONS_ATTR, dims));                                        
+    }
+    
+
+    private int[] m_Dimensions;
+
+    protected Array m_Data;
+
+    /// <summary>
+    /// Returns array dimensions
+    /// </summary>
+    public int[] Dimensions{ get{ return m_Dimensions;}}
+
+
+
+
+    public override void PerformSerializationTest(Serializer serializer, Stream target)
+    {
+      serializer.Serialize(m_Data, target);
+    }
+
+    public override void PerformDeserializationTest(Serializer serializer, Stream target)
+    {
+       var got = serializer.Deserialize(target) as Array;
+       if (got==null) Abort("Did not get an array back");
+       if (got.Length!=m_Data.Length) Abort("Length is different"); 
+    }
+
+  }
+}
---
+++ Source/Serbench/StockTests/ArrayTests.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+using NFX;
+using NFX.Environment;
+using NFX.Parsing;
+
+namespace Serbench.StockTests
+{
+
+
+  public class ArrayOfInt : ArrayTestBase
+  {
+    public ArrayOfInt(TestingSystem context, IConfigSectionNode conf) : base(context, conf) 
+    { 
+      m_Data = Array.CreateInstance(typeof(int), Dimensions);
+
+      NFX.Serialization.SerializationUtils.WalkArrayRead(m_Data, 
+       ()=> NFX.ExternalRandomGenerator.Instance.NextScaledRandomInteger(m_Min, m_Max)
+      );
+    }
+
+    [Config(Default=-1000)] private int m_Min;
+    [Config(Default=+1000)] private int m_Max;
+
+  }
+
+
+  public class ArrayOfNullableInt : ArrayTestBase
+  {
+    public ArrayOfNullableInt(TestingSystem context, IConfigSectionNode conf) : base(context, conf) 
+    { 
+      m_Data = Array.CreateInstance(typeof(int?), Dimensions);
+
+      NFX.Serialization.SerializationUtils.WalkArrayRead(m_Data, 
+       ()=> NFX.ExternalRandomGenerator.Instance.NextRandomInteger >0 ? (int?)null : NFX.ExternalRandomGenerator.Instance.NextScaledRandomInteger(m_Min, m_Max)
+      );
+    }
+
+    [Config(Default=-1000)] private int m_Min;
+    [Config(Default=+1000)] private int m_Max;
+
+  }
+
+
+  public class ArrayOfString : ArrayTestBase
+  {
+    public ArrayOfString(TestingSystem context, IConfigSectionNode conf) : base(context, conf) 
+    { 
+      m_Data = Array.CreateInstance(typeof(string), Dimensions);
+
+      NFX.Serialization.SerializationUtils.WalkArrayRead(m_Data, 
+       ()=> NFX.Parsing.NaturalTextGenerator.Generate( m_StringSize )
+      );
+    }
+
+    [Config]
+    private int m_StringSize;
+
+  }
+
+
+  public class ArrayOfDouble : ArrayTestBase
+  {
+    public ArrayOfDouble(TestingSystem context, IConfigSectionNode conf) : base(context, conf) 
+    { 
+      m_Data = Array.CreateInstance(typeof(double), Dimensions);
+
+      NFX.Serialization.SerializationUtils.WalkArrayRead(m_Data, 
+       ()=> NFX.ExternalRandomGenerator.Instance.NextRandomDouble
+      );
+    }
+
+
+  }
+
+
+  public class ArrayOfDecimal : ArrayTestBase
+  {
+    public ArrayOfDecimal(TestingSystem context, IConfigSectionNode conf) : base(context, conf) 
+    { 
+      m_Data = Array.CreateInstance(typeof(decimal), Dimensions);
+
+      NFX.Serialization.SerializationUtils.WalkArrayRead(m_Data, 
+       ()=> (decimal)NFX.ExternalRandomGenerator.Instance.NextRandomDouble
+      );
+    }
+
+
+  }
+
+
+}
