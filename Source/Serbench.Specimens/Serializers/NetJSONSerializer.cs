50a5241a82dce9c1845b60ab8b4e0554b0c4db02
Leonid Ganeline <leo.gan.57@gmail.com>
2015-06-17 21:49:30 -0700

    added NetJSON

---
+++ Source/Serbench.Specimens/Serializers/NetJSONSerializer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NFX;
+using NFX.Environment;
+
+using NetJSON;
+
+namespace Serbench.Specimens.Serializers
+{
+    /// <summary>
+    ///     Represents NetJSON:
+    /// See here https://github.com/rpgmaker/NetJSON
+    /// >PM Install-Package NetJSON 
+    /// </summary>
+    public class NetJSONSerializer : Serializer
+    {
+        private Type[] m_KnownTypes;
+        private Type m_primaryType;
+
+        public NetJSONSerializer(TestingSystem context, IConfigSectionNode conf)
+            : base(context, conf)
+        {
+            m_KnownTypes = ReadKnownTypes(conf);
+        }
+
+        public override void BeforeRuns(Test test)
+        {
+            var m_primaryType = test.GetPayloadRootType();
+        }
+
+        public override void Serialize(object root, Stream stream)
+        {
+            using (var sw = new StreamWriter(stream))
+            {
+                sw.Write(NetJSON.NetJSON.Serialize(m_primaryType, root));
+            }
+        }
+
+        public override object Deserialize(Stream stream)
+        {
+            using (var sr = new StreamReader(stream))
+            {
+                return NetJSON.NetJSON.Deserialize(m_primaryType, sr.ReadToEnd());
+            }
+        }
+
+        public override void ParallelSerialize(object root, Stream stream)
+        {
+             using (var sw = new StreamWriter(stream))
+            {
+                sw.Write(NetJSON.NetJSON.Serialize(m_primaryType, root));
+            }
+        }
+
+        public override object ParallelDeserialize(Stream stream)
+        {
+            using (var sr = new StreamReader(stream))
+            {
+                return NetJSON.NetJSON.Deserialize(m_primaryType, sr.ReadToEnd());
+            }
+        }
+    }
+}
