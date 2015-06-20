a84d6f7fce29f39899af9c5562e1561446a16079
leo-gan <leo.gan.57@gmail.com>
2015-06-20 14:37:54 -0700

    added Telemetry data class

---
+++ Source/Serbench.Specimens/Tests/Telemetry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using ProtoBuf;
+
+namespace Serbench.Specimens.Tests
+{
+    [ProtoContract] [DataContract] [Serializable] public class Telemetry
+    {
+         [ProtoMember(1)][DataMember] public string Id;
+         [ProtoMember(2)][DataMember] public DateTime TimeStamp;
+         [ProtoMember(3)][DataMember] public int Param1;
+         [ProtoMember(4)][DataMember] public int Param2;
+         [ProtoMember(5)][DataMember] public double[] Measurements;
+    }
+}
