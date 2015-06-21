7210e0c8600a94598c975a75dba3e4ebbb71b99b
sergey-msu <spmathf@gmail.com>
2015-06-21 20:22:26 +0300

    #21 overview report enhancements

--- Source/Serbench/WebViewer/scripts/serbench.js
+++ Source/Serbench/WebViewer/scripts/serbench.js
@@ -1,5 +1,8 @@
-function numberWithCommas(x) {
+function numberWithCommas(x, round) {
+    if (round)
+        x = x.toFixed(0);
+
     var parts = x.toString().split(".");
     parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     return parts.join(".");
-}
+};
