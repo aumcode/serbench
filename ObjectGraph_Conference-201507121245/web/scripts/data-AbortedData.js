a25d71bcd7b40c28d30ce3b7505a72053d042e9f
Dmitriy@ITAdapter <dmitriy@itadapter.com>
2015-07-13 18:43:08 -0400

    Adding more test results

---
+++ ObjectGraph_Conference-201507121245/web/scripts/data-AbortedData.js
@@ -0,0 +1,75 @@
+﻿//Java script file for Serbench Web output. Table 'AbortedData'
+var data_AbortedData = 
+[
+    {
+      "TestType": "Serbench.Specimens.Tests.ObjectGraph", 
+      "TestName": "Warmup ObjectGraph", 
+      "SerializerType": "Serbench.Specimens.Serializers.MessageSharkSerializer", 
+      "SerializerName": "MessageShark", 
+      "From": "ConfigSer", 
+      "Message": "Unhandled Exception: System.AccessViolationException: Attempted to read or write protected memory.\n This is often an indication that other memory is corrupt."
+    }, 
+    {
+      "TestType": "Serbench.Specimens.Tests.ObjectGraph", 
+      "TestName": "Conferences: 1; Participants: 250; Events: 10", 
+      "SerializerType": "Serbench.Specimens.Serializers.MessageSharkSerializer", 
+      "SerializerName": "MessageShark", 
+      "From": "ConfigSer", 
+      "Message": "Unhandled Exception: System.AccessViolationException: Attempted to read or write protected memory.\n This is often an indication that other memory is corrupt."
+    }, 
+    {
+      "TestType": "Serbench.Specimens.Tests.ObjectGraph", 
+      "TestName": "Conferences: 16; Participants: 16; Events: 5", 
+      "SerializerType": "Serbench.Specimens.Serializers.MessageSharkSerializer", 
+      "SerializerName": "MessageShark", 
+      "From": "ConfigSer", 
+      "Message": "Unhandled Exception: System.AccessViolationException: Attempted to read or write protected memory.\n This is often an indication that other memory is corrupt."
+    }, 
+    {
+      "TestType": "Serbench.Specimens.Tests.ObjectGraph", 
+      "TestName": "Warmup ObjectGraph", 
+      "SerializerType": "Serbench.Specimens.Serializers.NetJSONSerializer", 
+      "SerializerName": "NetJSONS", 
+      "From": "ConfigSer", 
+      "Message": "Generates zero payloads with unrealistic speeds (millions ops\/sec). Needs to be researched"
+    }, 
+    {
+      "TestType": "Serbench.Specimens.Tests.ObjectGraph", 
+      "TestName": "Conferences: 1; Participants: 250; Events: 10", 
+      "SerializerType": "Serbench.Specimens.Serializers.NetJSONSerializer", 
+      "SerializerName": "NetJSONS", 
+      "From": "ConfigSer", 
+      "Message": "Generates zero payloads with unrealistic speeds (millions ops\/sec). Needs to be researched"
+    }, 
+    {
+      "TestType": "Serbench.Specimens.Tests.ObjectGraph", 
+      "TestName": "Conferences: 16; Participants: 16; Events: 5", 
+      "SerializerType": "Serbench.Specimens.Serializers.NetJSONSerializer", 
+      "SerializerName": "NetJSONS", 
+      "From": "ConfigSer", 
+      "Message": "Generates zero payloads with unrealistic speeds (millions ops\/sec). Needs to be researched"
+    }, 
+    {
+      "TestType": "Serbench.Specimens.Tests.ObjectGraph", 
+      "TestName": "Warmup ObjectGraph", 
+      "SerializerType": "Serbench.Specimens.Serializers.SharpSerializer", 
+      "SerializerName": "SharpSerializer", 
+      "From": "ConfigSer", 
+      "Message": "Generates zero payloads with unrealistic speeds (millions ops\/sec). Needs to be researched"
+    }, 
+    {
+      "TestType": "Serbench.Specimens.Tests.ObjectGraph", 
+      "TestName": "Conferences: 1; Participants: 250; Events: 10", 
+      "SerializerType": "Serbench.Specimens.Serializers.SharpSerializer", 
+      "SerializerName": "SharpSerializer", 
+      "From": "ConfigSer", 
+      "Message": "Generates zero payloads with unrealistic speeds (millions ops\/sec). Needs to be researched"
+    }, 
+    {
+      "TestType": "Serbench.Specimens.Tests.ObjectGraph", 
+      "TestName": "Conferences: 16; Participants: 16; Events: 5", 
+      "SerializerType": "Serbench.Specimens.Serializers.SharpSerializer", 
+      "SerializerName": "SharpSerializer", 
+      "From": "ConfigSer", 
+      "Message": "Generates zero payloads with unrealistic speeds (millions ops\/sec). Needs to be researched"
+    }];
