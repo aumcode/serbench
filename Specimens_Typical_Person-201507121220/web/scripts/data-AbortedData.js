53942fd568a0acbacd836389cb718738784fddd9
Dmitriy@ITAdapter <dmitriy@itadapter.com>
2015-07-13 18:39:46 -0400

    Adding first test

---
+++ Specimens_Typical_Person-201507121220/web/scripts/data-AbortedData.js
@@ -0,0 +1,123 @@
+﻿//Java script file for Serbench Web output. Table 'AbortedData'
+var data_AbortedData = 
+[
+    {
+      "TestType": "Serbench.Specimens.Tests.TypicalPerson", 
+      "TestName": "Warmup TypicalPerson(single)", 
+      "SerializerType": "Serbench.Specimens.Serializers.MessageSharkSerializer", 
+      "SerializerName": "MessageShark", 
+      "From": "ConfigSer", 
+      "Message": "This serializer crashes the process without even throwing any exceptions"
+    }, 
+    {
+      "TestType": "Serbench.Specimens.Tests.TypicalPerson", 
+      "TestName": "Warmup TypicalPerson(list)", 
+      "SerializerType": "Serbench.Specimens.Serializers.MessageSharkSerializer", 
+      "SerializerName": "MessageShark", 
+      "From": "ConfigSer", 
+      "Message": "This serializer crashes the process without even throwing any exceptions"
+    }, 
+    {
+      "TestType": "Serbench.Specimens.Tests.TypicalPerson", 
+      "TestName": "TypicalPerson(single)", 
+      "SerializerType": "Serbench.Specimens.Serializers.MessageSharkSerializer", 
+      "SerializerName": "MessageShark", 
+      "From": "ConfigSer", 
+      "Message": "This serializer crashes the process without even throwing any exceptions"
+    }, 
+    {
+      "TestType": "Serbench.Specimens.Tests.TypicalPerson", 
+      "TestName": "TypicalPerson(list, 100)", 
+      "SerializerType": "Serbench.Specimens.Serializers.MessageSharkSerializer", 
+      "SerializerName": "MessageShark", 
+      "From": "ConfigSer", 
+      "Message": "This serializer crashes the process without even throwing any exceptions"
+    }, 
+    {
+      "TestType": "Serbench.Specimens.Tests.TypicalPerson", 
+      "TestName": "TypicalPerson(list, 500)", 
+      "SerializerType": "Serbench.Specimens.Serializers.MessageSharkSerializer", 
+      "SerializerName": "MessageShark", 
+      "From": "ConfigSer", 
+      "Message": "This serializer crashes the process without even throwing any exceptions"
+    }, 
+    {
+      "TestType": "Serbench.Specimens.Tests.TypicalPerson", 
+      "TestName": "Warmup TypicalPerson(single)", 
+      "SerializerType": "Serbench.Specimens.Serializers.NetJSONSerializer", 
+      "SerializerName": "NetJSONS", 
+      "From": "ConfigSer", 
+      "Message": "Generates zero payloads with unrealistic speeds (millions ops\/sec). Needs to be researched"
+    }, 
+    {
+      "TestType": "Serbench.Specimens.Tests.TypicalPerson", 
+      "TestName": "Warmup TypicalPerson(list)", 
+      "SerializerType": "Serbench.Specimens.Serializers.NetJSONSerializer", 
+      "SerializerName": "NetJSONS", 
+      "From": "ConfigSer", 
+      "Message": "Generates zero payloads with unrealistic speeds (millions ops\/sec). Needs to be researched"
+    }, 
+    {
+      "TestType": "Serbench.Specimens.Tests.TypicalPerson", 
+      "TestName": "TypicalPerson(single)", 
+      "SerializerType": "Serbench.Specimens.Serializers.NetJSONSerializer", 
+      "SerializerName": "NetJSONS", 
+      "From": "ConfigSer", 
+      "Message": "Generates zero payloads with unrealistic speeds (millions ops\/sec). Needs to be researched"
+    }, 
+    {
+      "TestType": "Serbench.Specimens.Tests.TypicalPerson", 
+      "TestName": "TypicalPerson(list, 100)", 
+      "SerializerType": "Serbench.Specimens.Serializers.NetJSONSerializer", 
+      "SerializerName": "NetJSONS", 
+      "From": "ConfigSer", 
+      "Message": "Generates zero payloads with unrealistic speeds (millions ops\/sec). Needs to be researched"
+    }, 
+    {
+      "TestType": "Serbench.Specimens.Tests.TypicalPerson", 
+      "TestName": "TypicalPerson(list, 500)", 
+      "SerializerType": "Serbench.Specimens.Serializers.NetJSONSerializer", 
+      "SerializerName": "NetJSONS", 
+      "From": "ConfigSer", 
+      "Message": "Generates zero payloads with unrealistic speeds (millions ops\/sec). Needs to be researched"
+    }, 
+    {
+      "TestType": "Serbench.Specimens.Tests.TypicalPerson", 
+      "TestName": "Warmup TypicalPerson(single)", 
+      "SerializerType": "Serbench.Specimens.Serializers.SharpSerializer", 
+      "SerializerName": "SharpSerializer", 
+      "From": "ConfigSer", 
+      "Message": "Generates zero payloads with unrealistic speeds (millions ops\/sec). Needs to be researched"
+    }, 
+    {
+      "TestType": "Serbench.Specimens.Tests.TypicalPerson", 
+      "TestName": "Warmup TypicalPerson(list)", 
+      "SerializerType": "Serbench.Specimens.Serializers.SharpSerializer", 
+      "SerializerName": "SharpSerializer", 
+      "From": "ConfigSer", 
+      "Message": "Generates zero payloads with unrealistic speeds (millions ops\/sec). Needs to be researched"
+    }, 
+    {
+      "TestType": "Serbench.Specimens.Tests.TypicalPerson", 
+      "TestName": "TypicalPerson(single)", 
+      "SerializerType": "Serbench.Specimens.Serializers.SharpSerializer", 
+      "SerializerName": "SharpSerializer", 
+      "From": "ConfigSer", 
+      "Message": "Generates zero payloads with unrealistic speeds (millions ops\/sec). Needs to be researched"
+    }, 
+    {
+      "TestType": "Serbench.Specimens.Tests.TypicalPerson", 
+      "TestName": "TypicalPerson(list, 100)", 
+      "SerializerType": "Serbench.Specimens.Serializers.SharpSerializer", 
+      "SerializerName": "SharpSerializer", 
+      "From": "ConfigSer", 
+      "Message": "Generates zero payloads with unrealistic speeds (millions ops\/sec). Needs to be researched"
+    }, 
+    {
+      "TestType": "Serbench.Specimens.Tests.TypicalPerson", 
+      "TestName": "TypicalPerson(list, 500)", 
+      "SerializerType": "Serbench.Specimens.Serializers.SharpSerializer", 
+      "SerializerName": "SharpSerializer", 
+      "From": "ConfigSer", 
+      "Message": "Generates zero payloads with unrealistic speeds (millions ops\/sec). Needs to be researched"
+    }];
